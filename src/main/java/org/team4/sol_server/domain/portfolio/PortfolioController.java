47dadf2469b7f648a2e476edee18297e96f48f61
dlrlfgh1210 <dlrlfgh517@gmail.com>
2025-03-11 09:56:00 +0900

    최신 뉴스정보 가져오기

--- src/main/java/org/team4/sol_server/domain/portfolio/PortfolioController.java
+++ src/main/java/org/team4/sol_server/domain/portfolio/PortfolioController.java
@@ -5,9 +5,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.team4.sol_server.domain.stock.category.StockService;
-
-import java.util.List;
 import java.util.Map;
 
 
--- src/main/java/org/team4/sol_server/domain/portfolio/PortfolioService.java
+++ src/main/java/org/team4/sol_server/domain/portfolio/PortfolioService.java
@@ -1,12 +1,10 @@
 package org.team4.sol_server.domain.portfolio;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.Arrays;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 @Service
