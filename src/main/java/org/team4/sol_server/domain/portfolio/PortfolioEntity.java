10ecf2cd5c4c07185e9092f3b1dd2ea0843e82ae
hyunseok99 <modesty66@naver.com>
2025-03-19 08:57:11 +0900

    [Modify] Portfolio Controller Request Param & Entity Modify

--- src/main/java/org/team4/sol_server/domain/portfolio/PortfolioEntity.java
+++ src/main/java/org/team4/sol_server/domain/portfolio/PortfolioEntity.java
@@ -17,29 +17,23 @@ public class PortfolioEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "krw_balance")
-    private Double krwBalance; // 보유 KRW (증권 계좌)
-
     @Column(name = "stock_name")
     private String stockName; // 보유 주식 이름
 
     @Column(name = "ticker")
     private String ticker; // 주식 Ticker
 
-    @Column(name = "stock_quantity")
-    private Double stockQuantity; // 보유 주식 수량
+    @Column(name = "stock_count")
+    private Double stockCount; // 보유 주식 수량
 
-    @Column(name = "average_purchase_price")
-    private Double averagePurchasePrice; // 매수 평균가
-
-    @Column(name = "purchase_amount")
-    private Double purchaseAmount; // 총 매수 금액
+    @Column(name = "average_price")
+    private Double averagePrice; // 매수 평균가
 
     @Column(name = "closing_price") // 전일 종가 컬럼
     private Double closingPrice;
 
-    @Column(name = "evaluation_amount") // 평가 금액 컬럼
-    private Double evaluationAmount;
+    @Column(name = "purchase_amount")
+    private Double purchaseAmount; // 총 매수 금액
 
     @Column(name = "profit_loss") // 평가 손익 컬럼
     private Double profitLoss;
